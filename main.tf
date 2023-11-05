85ec71aceecf91cefad0abed5ed0d716f68fd0de
sameerpjoshi <76880075+sameerpjoshi@users.noreply.github.com>
2023-11-05 23:05:31 +0530

    Changes in the alb

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id 
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     ex-http = {
@@ -74,12 +74,7 @@ module "alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80 
-        }
-      }
+      target_id        = aws_instance.blog.id 
     }
   }
 
