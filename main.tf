f8c91d113ffc5afd27b3afe88bf37933c8e6e14e
sameerpjoshi <76880075+sameerpjoshi@users.noreply.github.com>
2023-11-06 09:05:22 +0530

    changes for target groups and remove alb instance

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_groups.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -76,7 +76,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id 
     }
   }
 
